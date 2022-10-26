8be7241995eeb081a5cc4e579d55dab043ad04b3
Semih <semihemretureli@gmail.com>
2022-10-26 03:20:21 +0300

    Firebase register login added

--- app/src/main/java/com/semih/kaytolsayfasyapm/MainActivity.java
+++ app/src/main/java/com/semih/kaytolsayfasyapm/MainActivity.java
@@ -1,33 +1,46 @@
 package com.semih.kaytolsayfasyapm;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.semih.kaytolsayfasyapm.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText editTxtAd, editTxtSoyad, editTxtSehir, editTxtYas;
+    private EditText editTxtAd, editTxtSoyad, editTxtEmail, editTxtPassword;
     private CheckBox checkBox1, checkBox2, checkBox3;
     private RadioButton radioBtnE, radioBtnK;
 
-    private String editAd, editSoyad, editSehir, editYas;
+    private String editAd, editSoyad, editEmail, editPassword;
     private String checkTest1, checkTest2, checkTest3;
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
         //EditTextler//
         editTxtAd = (EditText) findViewById(R.id.editTxtAd);
         editTxtSoyad = (EditText) findViewById(R.id.editTxtSoyad);
-        editTxtSehir = (EditText) findViewById(R.id.editTxtSehir);
-        editTxtYas = (EditText) findViewById(R.id.editTxtYas);
+        editTxtEmail = (EditText) findViewById(R.id.editTxtSehir);
+        editTxtPassword = (EditText) findViewById(R.id.editTxtYas);
 
         //CheckBoxlar//
         checkBox1 = (CheckBox) findViewById(R.id.checkBoxHobi1);
@@ -42,14 +55,14 @@ public class MainActivity extends AppCompatActivity {
     public void btnKayıtOl(View v) {
         editAd = editTxtAd.getText().toString();
         editSoyad = editTxtSoyad.getText().toString();
-        editSehir = editTxtSehir.getText().toString();
-        editYas = editTxtYas.getText().toString();
+        editEmail = editTxtEmail.getText().toString();
+        editPassword = editTxtPassword.getText().toString();
 
-        if (!TextUtils.isEmpty(editAd) && !TextUtils.isEmpty(editSoyad) && !TextUtils.isEmpty(editSehir) && !TextUtils.isEmpty(editYas)) {
+        if (!TextUtils.isEmpty(editAd) && !TextUtils.isEmpty(editSoyad) && !TextUtils.isEmpty(editEmail) && !TextUtils.isEmpty(editPassword)) {
             System.out.println("Adınız : " + editAd);
             System.out.println("Soyadınız : " + editSoyad);
-            System.out.println("Şehriniz : " + editSehir);
-            System.out.println("Yaşınız : " + editYas);
+            System.out.println("Şehriniz : " + editEmail);
+            System.out.println("Yaşınız : " + editPassword);
 
             System.out.println("----Hobileriniz----");
             if (checkBox1.isChecked())
@@ -63,6 +76,27 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("Cinsiyetiniz : " + radioBtnE.getText().toString());
             else
                 System.out.println("Cinsiyetiniz : " + radioBtnK.getText().toString());
+
+            mAuth.createUserWithEmailAndPassword(editEmail, editPassword)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                //updateUI(user);
+                                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+                        }
+                    });
 
         }else
             System.out.println("Lütfen Bütün Bilgileri Doldurun");
